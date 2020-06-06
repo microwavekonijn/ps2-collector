025c843105bdbf1482a2485eb1ca48502a31e39d
Microwavekonijn <sjorsw@protonmail.ch>
2020-06-06 19:36:56 +0200

    Small change

--- src/config/collector.ts
+++ src/config/collector.ts
@@ -8,7 +8,6 @@ export default class Collector {
     public readonly subscribe: Array<SubscribeAction> = [{
         service: 'event',
         action: 'subscribe',
-        characters: ['all'],
         eventNames: [EventName.METAGAME_EVENT],
         worlds: ['all'],
         logicalAndCharactersWithWorlds: true,
