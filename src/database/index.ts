4223e50d80aece6f638467cbfc89f2015270e725
Microwavekonijn <sjorsw@protonmail.ch>
2020-06-06 15:08:24 +0200

    Fixes

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,6 @@ import Runnable, { RUNNABLE } from '../foundation/concerns/Runnable';
 import Connector from './Connector';
 import { Db, MongoClient } from 'mongodb';
 import config from '../config';
-import { createClient, RedisClient } from 'redis';
 
 export default new ContainerModule(bind => {
     bind<Runnable>(RUNNABLE).to(Connector);
@@ -14,9 +13,5 @@ export default new ContainerModule(bind => {
 
     bind(Db)
         .toDynamicValue(({container}) => container.get(MongoClient).db())
-        .inSingletonScope();
-
-    bind(RedisClient)
-        .toDynamicValue(() => createClient(config.database.redisUri))
         .inSingletonScope();
 });
