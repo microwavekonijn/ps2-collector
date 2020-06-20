ba52b8360ec2a02dd8b7fbe9f8ee8bb8aa90141e
Microwavekonijn <sjorsw@protonmail.ch>
2020-06-20 03:29:40 +0200

    Didn't mean to change that

--- src/config/collector.ts
+++ src/config/collector.ts
@@ -6,7 +6,7 @@ export default class Collector {
      * @type {SubscribeAction[]} Subscriptions that are made when starting the websocket
      */
     public readonly subscribe: Array<PS2ClientSubscription> = [{
-        eventNames: [EventName.ALL],
+        eventNames: [EventName.METAGAME_EVENT],
         worlds: ['all'],
     }];
 }
