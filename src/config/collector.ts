660cd91e3cdb604a487334e98277ce050b8b2956
Microwavekonijn <sjorsw@protonmail.ch>
2020-06-21 13:34:36 +0200

    Collect all

--- src/config/collector.ts
+++ src/config/collector.ts
@@ -1,4 +1,4 @@
-import EventName from '../collector/types/EventName';
+// import EventName from '../collector/types/EventName';
 import { PS2ClientSubscription } from '../census/utils/Types';
 
 export default class Collector {
@@ -6,7 +6,8 @@ export default class Collector {
      * @type {SubscribeAction[]} Subscriptions that are made when starting the websocket
      */
     public readonly subscribe: Array<PS2ClientSubscription> = [{
-        eventNames: [EventName.METAGAME_EVENT],
+        eventNames: ['all'],
+        characters: ['all'],
         worlds: ['all'],
     }];
 }
